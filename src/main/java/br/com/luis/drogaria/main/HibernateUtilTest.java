22e7600198a411b1ba92f9371a12f1a8298e978a
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-02-28 10:33:41 -0300

    Aula 140
    Aula do Sergio

---
+++ src/main/java/br/com/luis/drogaria/main/HibernateUtilTest.java
@@ -0,0 +1,13 @@
+package br.com.luis.drogaria.main;
+
+import org.hibernate.Session;
+
+import br.com.luis.drogaria.util.HibernateUtil;
+
+public class HibernateUtilTest {
+	public static void main(String[] args) {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		sessao.close();
+		HibernateUtil.getFabricaDeSessoes().close();
+	}
+}
