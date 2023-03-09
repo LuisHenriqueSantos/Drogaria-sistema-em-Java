78a0e82c8cfc4da02d82b869b90710b26288a8b6
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-03-09 17:30:26 -0300

    Aula 175

---
+++ src/main/java/br/com/luis/drogaria/bean/EstadoBean.java
@@ -0,0 +1,12 @@
+package br.com.luis.drogaria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class EstadoBean {
+	@SuppressWarnings("unused")
+	private void salvar(){
+		System.out.println("Teste");
+	}
+	
+}
