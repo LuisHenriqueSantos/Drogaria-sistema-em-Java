d346eea8750e70bafffe3598335e6c2479d59efb
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-03-10 13:36:27 -0300

    Aula - 179 - EstadoBean - Novo
    Nesta aula vimos que na tela do estado, ao clicar em novo, era para trazer a informação do que foi adicionados no campos (Nome, Sigla).

--- src/main/java/br/com/luis/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/luis/drogaria/bean/EstadoBean.java
@@ -2,12 +2,15 @@ package br.com.luis.drogaria.bean;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.view.facelets.FaceletContext;
 
 import org.jboss.logging.Messages;
 
 import com.mysql.cj.protocol.Message;
+
+import br.com.luis.drogaria.domain.Estado;
 
 /**
  * 
@@ -17,10 +20,24 @@ import com.mysql.cj.protocol.Message;
 
 @SuppressWarnings("unused")
 @ManagedBean
+@ViewScoped  
 public class EstadoBean {
 	@SuppressWarnings("unused")
+	private Estado estado; 
+	
+	public Estado getEstado() {
+		return estado;
+	}
+	
+	public Estado setEstado() {
+		return estado;
+	}
+		
+	public void novo() {
+		estado = new Estado();
+	}
 	public void salvar() {
-		org.omnifaces.util.Messages.addGlobalError("Nome e Sigla sem preencher ");
+		org.omnifaces.util.Messages.addGlobalInfo("Nome: " + estado.getNome() + "Sigla: " + estado.getSigla());
 		
 //		String texto = "Teste paro o botão salvar";
 //		FacesMessage menssagem = new FacesMessage(FacesMessage.SEVERITY_ERROR, texto, texto);
