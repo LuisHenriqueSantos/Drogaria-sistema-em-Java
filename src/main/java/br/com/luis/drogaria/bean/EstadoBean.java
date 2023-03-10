9ae28047613001b758a57910189d4ba8deba1f6f
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-03-10 16:05:01 -0300

    Aula 181 - EstadoBean - Salvar
    No EstadoBean.java
    Utilizamos o try, catch, caso der erro ao salvar o estado.
    
    Usamos errro.printStackTrace(); para exibir o logo do erro.
    
    No estados.xhtml no botão salvar usamos o update para limpar os campos do painel
    update=":mensagem :formCadastro:painel"

--- src/main/java/br/com/luis/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/luis/drogaria/bean/EstadoBean.java
@@ -7,11 +7,13 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.view.facelets.FaceletContext;
+import javax.management.RuntimeErrorException;
 
 import org.jboss.logging.Messages;
 
 import com.mysql.cj.protocol.Message;
 
+import br.com.luis.drogaria.dao.EstadoDAO;
 import br.com.luis.drogaria.domain.Estado;
 
 /**
@@ -22,25 +24,37 @@ import br.com.luis.drogaria.domain.Estado;
 
 @SuppressWarnings({ "unused", "serial" })
 @ManagedBean
-@ViewScoped  
+@ViewScoped
 public class EstadoBean implements Serializable {
 	@SuppressWarnings("unused")
-	private Estado estado; 
-	
+	private Estado estado;
+
 	public Estado getEstado() {
 		return estado;
 	}
-	
+
 	public Estado setEstado() {
 		return estado;
 	}
-		
+
 	public void novo() {
 		estado = new Estado();
 	}
+
 	public void salvar() {
-		org.omnifaces.util.Messages.addGlobalInfo("Nome: " + estado.getNome() + "Sigla: " + estado.getSigla());
-		
+		try {
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estadoDAO.salvar(estado);
+
+			novo();
+
+			org.omnifaces.util.Messages.addGlobalInfo("Estado salvo com sucesso");
+
+			throw new RuntimeException("Erro forçado");
+		} catch (RuntimeException errro) {
+			org.omnifaces.util.Messages.addGlobalInfo("Ocorrreu um erro!");
+			errro.printStackTrace();
+		}
 //		String texto = "Teste paro o botão salvar";
 //		FacesMessage menssagem = new FacesMessage(FacesMessage.SEVERITY_ERROR, texto, texto);
 //
