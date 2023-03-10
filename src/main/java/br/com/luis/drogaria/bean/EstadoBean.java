d9a2980a7f29716d857dde765dc0b1956c1e9f4c
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-03-10 09:42:43 -0300

    Aula 176 - FacesMessage
    Professor Sergio -  Nesta aula usamos o FacesMessage para exibir uma mensagem ao clicar no botão salvar

--- src/main/java/br/com/luis/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/luis/drogaria/bean/EstadoBean.java
@@ -1,12 +1,26 @@
 package br.com.luis.drogaria.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.view.facelets.FaceletContext;
 
+/**
+ * 
+ * @author ljesus
+ *
+ */
+
+@SuppressWarnings("unused")
 @ManagedBean
 public class EstadoBean {
 	@SuppressWarnings("unused")
-	public void salvar(){
-		System.out.println("Teste paro o botão salvar");
+	public void salvar() {
+		String texto = "Teste paro o botão salvar";
+		FacesMessage menssagem = new FacesMessage(FacesMessage.SEVERITY_ERROR, texto, texto);
+
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, menssagem);
 	}
-	
+
 }
