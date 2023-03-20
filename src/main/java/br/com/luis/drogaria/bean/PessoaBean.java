4421395245a00c71b4e230222b74ff613aabe9bf
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-03-20 16:47:23 -0300

    Aula 204 - PessoaBean - Novo
    Professor Sergio
    
    Nessa aula trabalhamos na tela cadastro de pessoas.
    No botão NOVO, trabalhando na chave estrangeria estado, capturando as informações do banco de dados e trazendo em tela.

--- src/main/java/br/com/luis/drogaria/bean/PessoaBean.java
+++ src/main/java/br/com/luis/drogaria/bean/PessoaBean.java
@@ -1,6 +1,7 @@
 package br.com.luis.drogaria.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -10,10 +11,11 @@ import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
-import antlr.debug.Event;
 import br.com.luis.drogaria.dao.CidadeDAO;
+import br.com.luis.drogaria.dao.EstadoDAO;
 import br.com.luis.drogaria.dao.PessoaDAO;
 import br.com.luis.drogaria.domain.Cidade;
+import br.com.luis.drogaria.domain.Estado;
 import br.com.luis.drogaria.domain.Pessoa;
 
 @SuppressWarnings("serial")
@@ -23,9 +25,27 @@ import br.com.luis.drogaria.domain.Pessoa;
 public class PessoaBean implements Serializable {
 	private Pessoa pessoa;
 	private Cidade cidade;
+	private Estado estado;
 
+	private List<Estado> estados;
 	private List<Cidade> cidades;
 	private List<Pessoa> pessoas;
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
 
 	public List<Cidade> getCidades() {
 		return cidades;
@@ -74,9 +94,11 @@ public class PessoaBean implements Serializable {
 	public void novo() {
 		try {
 			pessoa = new Pessoa();
-
-			CidadeDAO cidadeDAO = new CidadeDAO();
-			cidades = cidadeDAO.listar();
+			
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+			
+			cidades = new ArrayList<Cidade>();
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao gerar uma nova pessoa!");
 		}
