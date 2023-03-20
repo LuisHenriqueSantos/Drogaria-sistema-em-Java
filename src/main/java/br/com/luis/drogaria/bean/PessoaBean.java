f5a18eb3206da8757118fff6c7f1f4bc4ba6554d
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-03-20 10:56:43 -0300

    Aula 203 - PessoaBean - Listar
    Professor Sergio
    
    Nessa aula fizemos o método listar.
    Pegando as informações do banco e sendo exibido em tala para o cliente.

---
+++ src/main/java/br/com/luis/drogaria/bean/PessoaBean.java
@@ -0,0 +1,98 @@
+package br.com.luis.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+
+import org.omnifaces.util.Messages;
+
+import antlr.debug.Event;
+import br.com.luis.drogaria.dao.CidadeDAO;
+import br.com.luis.drogaria.dao.PessoaDAO;
+import br.com.luis.drogaria.domain.Cidade;
+import br.com.luis.drogaria.domain.Pessoa;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+
+public class PessoaBean implements Serializable {
+	private Pessoa pessoa;
+	private Cidade cidade;
+
+	private List<Cidade> cidades;
+	private List<Pessoa> pessoas;
+
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+
+	@PostConstruct
+	public void listar() {
+		try {
+			PessoaDAO pessoaDAO = new PessoaDAO();
+			pessoas = pessoaDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar as pessoas!");
+			erro.printStackTrace();
+		}
+
+	}
+
+	public void novo() {
+		try {
+			pessoa = new Pessoa();
+
+			CidadeDAO cidadeDAO = new CidadeDAO();
+			cidades = cidadeDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao gerar uma nova pessoa!");
+		}
+
+	}
+
+	public void editar(ActionEvent evento) {
+
+	}
+
+	public void salvar() {
+
+	}
+
+	public void excluir(ActionEvent evento) {
+
+	}
+
+}
