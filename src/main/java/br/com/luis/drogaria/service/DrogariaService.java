7410069202bcb06b3b2b4c7a4bd3616d3bbd5505
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-03-29 17:28:00 -0300

    Aula 218 - FabricanteService - Listar - falta terminar
    Professor Sergio
    
    Dar continuidade amanhã, devido horário.

--- src/main/java/br/com/luis/drogaria/service/DrogariaService.java
+++ src/main/java/br/com/luis/drogaria/service/DrogariaService.java
@@ -3,7 +3,7 @@ package br.com.luis.drogaria.service;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-// http://:localhost:8080/Drogaria/rest/drogaria
+// http://localhost:8080/Drogaria/rest/drogaria
 
 @Path("drogaria")
 public class DrogariaService {
---
+++ src/main/java/br/com/luis/drogaria/service/FabricanteService.java
@@ -0,0 +1,27 @@
+package br.com.luis.drogaria.service;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import com.google.gson.Gson;
+
+import br.com.luis.drogaria.dao.FabricanteDAO;
+import br.com.luis.drogaria.domain.Fabricante;
+
+//http://127.0.0.1:8080/Drogaria/rest/fabricante
+@Path("fabricante")
+
+public class FabricanteService {
+	@GET
+	public String listar() {
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		List<Fabricante> fabricantes = fabricanteDAO.listar("descricao");
+
+		Gson gson = new Gson();
+		String json = gson.toJson(fabricantes);
+
+		return json;
+	}
+}
