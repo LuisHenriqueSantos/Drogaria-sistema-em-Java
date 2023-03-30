1f471ae9c003c6198f6ca3aa10492074df202d4f
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-03-30 17:25:58 -0300

    Aula 221 - FabricanteService - Salvar
    Professor Sergio
    
    Nessa aulas trabalhamos com o parametro POST usando o Postman

--- src/main/java/br/com/luis/drogaria/service/FabricanteService.java
+++ src/main/java/br/com/luis/drogaria/service/FabricanteService.java
@@ -3,11 +3,13 @@ package br.com.luis.drogaria.service;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
 import com.google.gson.Gson;
 
+import antlr.collections.Stack;
 import br.com.luis.drogaria.dao.FabricanteDAO;
 import br.com.luis.drogaria.domain.Fabricante;
 
@@ -30,7 +32,7 @@ public class FabricanteService {
 	// http://127.0.0.1:8080/Drogaria/rest/fabricante/10
 	@GET
 	@Path("{codigo}")
-	public String buscar(@PathParam ("codigo") Long codigo) {
+	public String buscar(@PathParam("codigo") Long codigo) {
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		Fabricante fabricante = fabricanteDAO.buscar(codigo);
 
@@ -39,6 +41,19 @@ public class FabricanteService {
 
 		return json;
 
+	}
+
+	// http://127.0.0.1:8080/Drogaria/rest/fabricante
+	@POST
+	public String salvar(String json) {
+		Gson gson = new Gson();
+		Fabricante fabricante = gson.fromJson(json, Fabricante.class);
+
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		fabricanteDAO.merge(fabricante);
+		
+		String jsonSaida = gson.toJson(fabricante);
+		return jsonSaida;
 	}
 
 	public void editar() {
