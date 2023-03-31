ff842baf8a240fe63e684784add1d413b38b1fe8
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-03-31 10:47:39 -0300

    Aula 222 - FabricanteService - Editar
    Professor Sergio
    
    Nessa aula trabalhamos para fazer a edição usando o método PUT e trabalhando em conjunto com o Postman.
    
    OBS:
    Foi corrigido também a tela de Usuarioxhtml, onde ao clicar no icone excluir não chamava a ação do método excluir.
    Faltava criar o <p:confirmDialog.

--- src/main/java/br/com/luis/drogaria/service/FabricanteService.java
+++ src/main/java/br/com/luis/drogaria/service/FabricanteService.java
@@ -2,14 +2,15 @@ package br.com.luis.drogaria.service;
 
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
 import com.google.gson.Gson;
 
-import antlr.collections.Stack;
 import br.com.luis.drogaria.dao.FabricanteDAO;
 import br.com.luis.drogaria.domain.Fabricante;
 
@@ -51,16 +52,33 @@ public class FabricanteService {
 
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		fabricanteDAO.merge(fabricante);
-		
+
 		String jsonSaida = gson.toJson(fabricante);
 		return jsonSaida;
 	}
 
-	public void editar() {
+	@PUT
+	public String editar(String json) {
+		Gson gson = new Gson();
+		Fabricante fabricante = gson.fromJson(json, Fabricante.class);
 
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		fabricanteDAO.editar(fabricante);
+
+		String jsonSaida = gson.toJson(fabricante);
+		return jsonSaida;
 	}
 
-	public void excluir() {
+	@DELETE
+	public String excluir(String json) {
+		Gson gson = new Gson();
+		Fabricante fabricante = gson.fromJson(json, Fabricante.class);
+
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		fabricanteDAO.excluir(fabricante);
+
+		String jsonSaida = gson.toJson(fabricante);
+		return jsonSaida;
 
 	}
 
