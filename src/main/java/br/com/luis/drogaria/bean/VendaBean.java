e12c99eca3c9b5d6f23b09cb41309698074c0fe4
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-04-06 17:26:53 -0300

    Aula 231 VendaBean
    Professor Sergio.
    
    Ainda faltar tarminar

---
+++ src/main/java/br/com/luis/drogaria/bean/VendaBean.java
@@ -0,0 +1,13 @@
+package br.com.luis.drogaria.bean;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+
+public class VendaBean implements Serializable {
+}
