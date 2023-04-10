10b00ba000dc5140b1b997ac3c85c82aa54cdbb1
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-04-10 15:46:42 -0300

    Aula 233 - Adicionando Produtos
    Professor Sergio
    
    Nessa aula trabalhamos para estar adicionando os itens na cesta de compras.

--- src/main/java/br/com/luis/drogaria/bean/VendaBean.java
+++ src/main/java/br/com/luis/drogaria/bean/VendaBean.java
@@ -1,12 +1,15 @@
 package br.com.luis.drogaria.bean;
 
 import br.com.luis.drogaria.dao.ProdutoDAO;
+import br.com.luis.drogaria.domain.ItemVenda;
 import br.com.luis.drogaria.domain.Produtos;
 import org.omnifaces.util.Messages;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("serial")
@@ -15,6 +18,7 @@ import java.util.List;
 
 public class VendaBean implements Serializable {
     private List<Produtos> produtos;
+    private List<ItemVenda> itemVendas;
 
     public List<Produtos> getProdutos() {
         return produtos;
@@ -24,14 +28,36 @@ public class VendaBean implements Serializable {
         this.produtos = produtos;
     }
 
+    public List<ItemVenda> getItemVendas() {
+        return itemVendas;
+    }
+
+    public void setItemVendas(List<ItemVenda> itemVendas) {
+        this.itemVendas = itemVendas;
+    }
+
     @PostConstruct
     public void listar() {
         try {
             ProdutoDAO produtoDAO = new ProdutoDAO();
             produtos = produtoDAO.listar();
+            itemVendas = new ArrayList<>();
         } catch (RuntimeException erro) {
             Messages.addGlobalError("Erro ao carregar a tela de venda!");
             erro.printStackTrace();
         }
     }
+
+    public void adcionarItem(ActionEvent evento) {
+        Produtos produto = (Produtos) evento.getComponent().getAttributes().get("produtoSelecionado");
+        System.out.println(produto);
+
+        ItemVenda itemVenda = new ItemVenda();
+        itemVenda.setValorParcial(produto.getPreco());
+        itemVenda.setProduto(produto);
+        itemVenda.setQuantidade(new Short("1"));
+
+        itemVendas.add(itemVenda);
+    }
+
 }
