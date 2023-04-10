6ae6de69f483285a547f4f89dd7a6bbe09fb7d42
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-04-10 11:01:49 -0300

    Aula 232 - Venda - Listagem de produtos
    Professor Sergio
    
    Nessa aula fizemos o carregamento dos produtos.

--- src/main/java/br/com/luis/drogaria/bean/VendaBean.java
+++ src/main/java/br/com/luis/drogaria/bean/VendaBean.java
@@ -1,13 +1,37 @@
 package br.com.luis.drogaria.bean;
 
-
+import br.com.luis.drogaria.dao.ProdutoDAO;
+import br.com.luis.drogaria.domain.Produtos;
+import org.omnifaces.util.Messages;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import java.io.Serializable;
+import java.util.List;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
 
 public class VendaBean implements Serializable {
+    private List<Produtos> produtos;
+
+    public List<Produtos> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produtos> produtos) {
+        this.produtos = produtos;
+    }
+
+    @PostConstruct
+    public void listar() {
+        try {
+            ProdutoDAO produtoDAO = new ProdutoDAO();
+            produtos = produtoDAO.listar();
+        } catch (RuntimeException erro) {
+            Messages.addGlobalError("Erro ao carregar a tela de venda!");
+            erro.printStackTrace();
+        }
+    }
 }
